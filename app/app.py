68321fe16041cb6655e1a5633d6bbf459a9aba90
Likit Preeyanon <preeyano@msu.edu>
2017-07-26 12:59:51 +0700

    added another mongodb to store results

--- app/app.py
+++ app/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 app.config['MONGO_HOST'] = 'localhost'
 app.config['MONGO_PORT'] = 27017
 app.config['MONGO_DBNAME'] = 'hsudb'
-
 mongo = PyMongo(app)
+
+app.config['MONGO2_HOST'] = 'localhost'
+app.config['MONGO2_PORT'] = 27017
+app.config['MONGO2_DBNAME'] = 'survey_dementia01'
+mongo2 = PyMongo(app, config_prefix='MONGO2')
